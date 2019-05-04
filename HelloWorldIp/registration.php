3d85216a8cd389749b37fba70226d0ffbd464118
Lucas Clazer <lucas.clazer@gmail.com>
2019-05-04 13:35:29 -0300

    Correção no nome do modulo.

--- HelloWorldIp/registration.php
+++ HelloWorldIp/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MagentoLucasModules_HelloWordIp',
+    'MagentoLucasModules_HelloWorldIp',
     __DIR__
 );
