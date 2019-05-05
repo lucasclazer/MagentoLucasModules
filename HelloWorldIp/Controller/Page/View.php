0926e5f45685418b7981fb30a3243bfe002e1692
Lucas Clazer <lucas.clazer@gmail.com>
2019-05-04 21:33:28 -0300

    *Encontrado solução
    Adicionado função que retorna endereço de ip remoto.

--- HelloWorldIp/Controller/Page/View.php
+++ HelloWorldIp/Controller/Page/View.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 
 namespace MagentoLucasModules\HelloWorldIp\Controller\Page;
-
 
 class View extends \Magento\Framework\App\Action\Action
 {
@@ -16,10 +15,10 @@ class View extends \Magento\Framework\App\Action\Action
     public function __construct(
        \Magento\Framework\App\Action\Context $context,
        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory)
-{
+    {
        $this->resultJsonFactory = $resultJsonFactory;
        parent::__construct($context);
-}
+    }
     /**
      * View  page action
      *
@@ -27,8 +26,43 @@ class View extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-       $result = $this->resultJsonFactory->create();
-       $data = ['message' => 'Hello world!', 'IP:' => Mage::helper('core/http')->getRemoteAddr()];
+            //IDEs utilizadas
+            //VSCode - inicialmente
+            //PHPStorm
 
-return $result->setData($data);
+
+            //Configuração utilizada
+            //https://bitnami.com/stack/magento
+            // vmware / Debian
+            // Php 7.1 + Zend + Xdebug
+                // PHP 7.1.27 (cli) (built: Apr  3 2019 17:37:46) ( NTS )
+                // Copyright (c) 1997-2018 The PHP Group
+                // Zend Engine v3.1.0, Copyright (c) 1998-2018 Zend Technologies
+                // with Zend OPcache v7.1.27, Copyright (c) 1999-2018, by Zend Technologies
+                // with Xdebug v2.7.1, Copyright (c) 2002-2019, by Derick Rethans
+            // xdebug configurado posteriormente.
+            //Magento 2.2
+
+
+            //Sites de referências:
+
+            //https://xdebug.org/
+            //https://stackoverflow.com/
+            //https://bitnami.com/stack/magento
+            //https://www.tutorialsplane.com/magento-2-get-ip-address/
+            //https://devdocs.magento.com/guides/
+            //https://mage2gen.com/
+            //https://www.jetbrains.com/help/phpstorm/2019.1/configuring-remote-interpreters.html
+            //https://belvg.com/blog/controllers-routers-and-responses-in-magento-2.html
+
+            /** @var \Magento\Framework\ObjectManagerInterface */
+            $objctManager = \Magento\Framework\App\ObjectManager::getInstance();
+            /** @var \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress $a */
+            $remote = $objctManager->get('Magento\Framework\HTTP\PhpEnvironment\RemoteAddress');
+
+            $result = $this->resultJsonFactory->create();
+
+            $data = ['Teste Inicial:' => 'Hello world!', 'IP:' => $remote->getRemoteAddress()];
+
+      return $result->setData($data);
 } }
